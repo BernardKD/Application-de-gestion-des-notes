bd714ed333ed1762121755c6fd89afbb61b5b823
BernardKD <155765608+BernardKD@users.noreply.github.com>
2024-07-04 02:27:51 +0100

    Update Connect.java

--- Connect.java
+++ Connect.java
@@ -1,24 +1,24 @@
-
-package Etudiant;
+package SuperProf;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 
-
 public class Connect {
+    
     Connection cn;
-    public Connect(){
+    public Connect(String datab, String userx, String passx){
         try{
             Class.forName("com.mysql.cj.jdbc.Driver");
-            cn = (Connection) DriverManager.getConnection("jdbc:mysql://localhost/livre","bernard","Passer@123");
+            cn = (Connection) DriverManager.getConnection("jdbc:mysql://localhost/"+datab,userx,passx);
             System.out.println("Connection Etablie");
         }catch(Exception e){
             System.out.println("Erreur de connection");
             e.printStackTrace();
         }
+        
     }
     public Connection maConnection(){
         return cn;
     }
+    
 }
-
